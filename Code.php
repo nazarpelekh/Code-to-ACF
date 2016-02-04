a071db97bcdd2152ee8dd5a49d7182b31e227278
Nazar <nazar_peleh@meta.ua>
2016-02-04 15:46:10 +0200

    Ready Code To ACF

--- Code.php
+++ Code.php
@@ -3,21 +3,16 @@
     <?php while ( has_sub_field('flex_name') ): ?>
 
         <?php if ( get_row_layout() == 'sub_field_name' ): ?>
-            <div class="post" style="background: <?php the_sub_field('bg'); ?>;">
-                <h4><?php the_sub_field('title'); ?></h4>
-                <?php the_sub_field('text'); ?>
+            <div class="item">
+                <?php the_sub_field('something'); ?>
             </div>
         <?php elseif(get_row_layout() == 'sub_field_name'): ?>
-            <div class="post" style="<?php echo image_src(get_sub_field('bg'), 'post', true); ?>">
-                <h4><?php the_sub_field('title'); ?></h4>
-                <?php the_sub_field('text'); ?>
-                <a href="<?php the_sub_field('link'); ?>" target="_blank">Get insights</a>
-                <div class="like"></div>
+            <div class="item">
+                <?php the_sub_field('something'); ?>
             </div>
         <?php elseif(get_row_layout() == 'sub_field_name'): ?>
-            <div class="large_post" style="<?php echo image_src(get_sub_field('bg'), 'large_post', true); ?>">
-                <h4><?php the_sub_field('title'); ?></h4>
-                <?php the_sub_field('text'); ?>
+            <div class="item">
+                <?php the_sub_field('something'); ?>
             </div>
         <?php endif; ?>
 
@@ -31,8 +26,7 @@
     <div class="inner">
         <?php while(has_sub_field('repeater_name')): ?>
             <div class="item">
-                <strong><?php the_sub_field('bold'); ?></strong>
-                <?php the_sub_field('content'); ?>
+                <?php the_sub_field('something'); ?>
             </div>
         <?php endwhile; ?>
     </div>
